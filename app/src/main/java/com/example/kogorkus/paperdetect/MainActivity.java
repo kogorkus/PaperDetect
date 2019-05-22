6a50e868394aa233d630c2aa32513e10c527dcfb
kogorkus <smitsmit134@mail.ru>
2019-05-22 13:34:11 +0300

    def3

--- app/src/main/java/com/example/kogorkus/paperdetect/MainActivity.java
+++ app/src/main/java/com/example/kogorkus/paperdetect/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.kogorkus.paperdetect;
 
+
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -16,22 +20,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
 
-import java.util.Locale;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseDatabase database;
     private TextView textView;
-    private int NumberOfTerms;
-    private double Difference;
-    private double TotalLength;
 
 
     @Override
@@ -40,53 +42,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.MainText);
 
+        FirebaseMessaging.getInstance().subscribeToTopic("News")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "subscribed";
+                        if (!task.isSuccessful()) {
+                            msg = "subscribe failed";
+                        }
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        database = FirebaseDatabase.getInstance();
-        DatabaseReference lengthRef = database.getReference("length");
-        lengthRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String value = Objects.requireNonNull(dataSnapshot.getValue(String.class));
-                if (!value.equals("")) {
-                    TotalLength = Double.parseDouble(value);
-                    NumberOfTerms = (int) (TotalLength * 100 / ((14 + 35) * 0.5));
-                    Difference = ((35 - 14) / (double) NumberOfTerms);
-                    textView.setText(value);
-                } else {
-                    textView.setText(R.string.PleaseSet);
+        if(isOnline()) {
+            database = FirebaseDatabase.getInstance();
+            DatabaseReference lengthRef = database.getReference("ESP8266_Test");
+            lengthRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String lengthValue = dataSnapshot.child("CurrentLength").getValue(String.class);
+                    String percentValue = dataSnapshot.child("Percent").getValue(String.class);
+                    if (!lengthValue.equals("") && !lengthValue.equals("0.00")) {
+                        textView.setText(lengthValue + " м");
+                        if(!percentValue.equals("NaN"))
+                        {
+                            textView.append("\n (" + percentValue + "%)");
+                        }
+                    } else {
+                        textView.setText(R.string.PleaseSet);
+                    }
+                    Log.d("db", "Value is: " + lengthValue);
                 }
-                Log.d("db", "Value is: " + value);
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.w("db", "Failed to read value.", error.toException());
-            }
-        });
-
-        DatabaseReference turnRef = database.getReference("ESP8266_Test/Data");
-        turnRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                Long value = dataSnapshot.getValue(Long.class);
-                if (value != 0 && TotalLength != 0) {
-                    double var = TotalLength * 100 - (value * 0.5 * (35 + (35 + (value - 1) * -Difference)));
-                    textView.setText(String.format(Locale.getDefault(), "%.2f", var / 100));
-                    textView.append("\n" + "(" + (String.format(Locale.getDefault(), "%.2f", var / TotalLength)) + "%)");
-                    Log.d("db", "Value is: " + value);
-                } else {
-                    textView.setText(R.string.PleaseSet);
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.w("db", "Failed to read value.", error.toException());
                 }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.w("db", "Failed to read value.", error.toException());
-            }
-        });
+            });
+        }
+        else
+        {
+            textView.setText("no Internet connection");
+        }
     }
 
     public void OpenListActivity(View view) {
@@ -107,8 +109,7 @@ public class MainActivity extends AppCompatActivity {
         }
         String length = data.getStringExtra("length");
         textView.setText(length);
-        DatabaseReference myRef = database.getReference("length");
-        myRef.setValue(length);
+        SetLength(length);
     }
 
     public void ShowManuallyDialog(View view) {
@@ -125,8 +126,7 @@ public class MainActivity extends AppCompatActivity {
                             String length = userInput.getText() + "";
                             Double.parseDouble(length);
                             textView.setText(length);
-                            DatabaseReference myRef = database.getReference("length");
-                            myRef.setValue(length);
+                            SetLength(length);
                         } catch (NumberFormatException e) {
                             Toast.makeText(MainActivity.this, "Please enter correct number", Toast.LENGTH_SHORT).show();
                         }
@@ -151,12 +151,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.remove_length) {
-            DatabaseReference myRef = database.getReference("length");
-            myRef.setValue("");
-            myRef = database.getReference("ESP8266_Test/Data");
+            DatabaseReference myRef = database.getReference("ESP8266_Test/Data");
             myRef.setValue(0);
+            SetLength("");
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+    public void SetLength(String length)
+    {
+        DatabaseReference myRef = database.getReference("ESP8266_Test/Length");
+        myRef.setValue(length);
+        myRef = database.getReference("ESP8266_Test/CurrentLength");
+        myRef.setValue(length);
     }
 }
