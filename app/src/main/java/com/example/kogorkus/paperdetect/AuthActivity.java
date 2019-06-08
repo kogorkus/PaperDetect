7372c26e08727690348959c8238da173106dc20f
kogorkus <smitsmit134@mail.ru>
2019-06-08 14:39:59 +0300

    with Auth

---
+++ app/src/main/java/com/example/kogorkus/paperdetect/AuthActivity.java
@@ -0,0 +1,109 @@
+package com.example.kogorkus.paperdetect;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class AuthActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText PassET, MailET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        PassET = findViewById(R.id.PassET);
+        MailET = findViewById(R.id.MailET);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    if (user.isEmailVerified()) {
+                        Intent intent = new Intent(AuthActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            }
+        };
+
+        mAuth.addAuthStateListener(mAuthListener);
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null)
+        {
+            Intent intent = new Intent(AuthActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
+
+    public void SignUp(View view) {
+        if(!MailET.getText().toString().equals("") && !PassET.getText().toString().equals(""))
+            mAuth.createUserWithEmailAndPassword(MailET.getText().toString(), PassET.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(AuthActivity.this, "Signing up successful", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            user.sendEmailVerification();
+                            Toast.makeText(AuthActivity.this, "Please, verify your Email", Toast.LENGTH_LONG).show();
+                        } else {
+
+                            Toast.makeText(AuthActivity.this, "Signing up failed." +
+                                            task.getException().toString()
+                                            .split(":")[1],
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
+    public void SignIn(View view) {
+        if(!MailET.getText().toString().equals("") && !PassET.getText().toString().equals(""))
+            mAuth.signInWithEmailAndPassword(MailET.getText().toString(), PassET.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                            if (task.isSuccessful()) {
+                                if(user.isEmailVerified()) {
+                                    Toast.makeText(AuthActivity.this, "Signing in successful",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                                else {
+                                    Toast.makeText(AuthActivity.this, "Email is not Verified! Check your Email",
+                                            Toast.LENGTH_LONG).show();
+                                }
+
+                            } else {
+                                Toast.makeText(AuthActivity.this, "Signing in failed." +
+                                                task.getException().toString().split(":")[1],
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+
+                });
+    }
+}
--- app/src/main/java/com/example/kogorkus/paperdetect/MainActivity.java
+++ app/src/main/java/com/example/kogorkus/paperdetect/MainActivity.java
@@ -22,6 +22,8 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -34,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseDatabase database;
     private TextView textView;
+    private FirebaseAuth mAuth;
+
 
 
     @Override
@@ -41,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.MainText);
+
+        mAuth = FirebaseAuth.getInstance();
 
         FirebaseMessaging.getInstance().subscribeToTopic("News")
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -59,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        if(isOnline()) {
             database = FirebaseDatabase.getInstance();
             DatabaseReference lengthRef = database.getReference("ESP8266_Test");
             lengthRef.addValueEventListener(new ValueEventListener() {
@@ -84,11 +89,7 @@ public class MainActivity extends AppCompatActivity {
                     Log.w("db", "Failed to read value.", error.toException());
                 }
             });
-        }
-        else
-        {
-            textView.setText("no Internet connection");
-        }
+
     }
 
     public void OpenListActivity(View view) {
@@ -155,16 +156,22 @@ public class MainActivity extends AppCompatActivity {
             myRef.setValue(0);
             SetLength("");
         }
+        else if (item.getItemId() == R.id.sign_out)
+        {
+            mAuth.signOut();
+            finish();
+            startActivity(new Intent(MainActivity.this, AuthActivity.class));
+
+        }
+        else if (item.getItemId() == R.id.credits)
+        {
+
+        }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public boolean isOnline() {
-        ConnectivityManager cm =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        return netInfo != null && netInfo.isConnectedOrConnecting();
-    }
+
     public void SetLength(String length)
     {
         DatabaseReference myRef = database.getReference("ESP8266_Test/Length");
